d4f9e8d7a30be0390ab3c8fa4cbb22e50d10684c
Matthew Perkins <matthewhperkins@gmail.com>
2014-05-30 16:44:44 -0400

    composite accepts .atf IV file, prints pars to txt file

--- DynClmpComposite.py
+++ DynClmpComposite.py
@@ -1,4 +1,4 @@
-from traits.api import HasTraits, Instance, Int, Float, on_trait_change
+from traits.api import HasTraits, Instance, Int, Float, Array, on_trait_change, Button
 from traitsui.api import View, Group, Item
 from enable.api import ColorTrait
 from enable.component_editor import ComponentEditor
@@ -6,21 +6,23 @@ from chaco.api import marker_trait, Plot, ArrayPlotData, VPlotContainer
 from numpy import linspace, sin
 import pdb
 import numpy as np
+import sys
 
 class HHCurrentTraits(HasTraits):
 
-    gateplot = Instance(VPlotContainer)
-    ivplot = Instance(Plot)
+    plots = Instance(VPlotContainer)
+    # write_button = Instance(Button)
+    write_button = Button()
 
-    ErevCur1 = Float(-60)
+    ErevCur1 = Float(20)
     GmaxCur1 = Float(0.05)
-    MvhalfCur1 = Float(-80)
-    MKCur1 = Float(10)
+    MvhalfCur1 = Float(50)
+    MKCur1 = Float(-80)
     MssminCur1 = Float(0)
     Mdenom_expCur1 = Float(1)
     MpCur1 = Float(1)
 
-    NvhalfCur1 = Float(-80)
+    NvhalfCur1 = Float(25)
     NKCur1 = Float(10)
     NssminCur1 = Float(0)
     Ndenom_expCur1 = Float(1)
@@ -30,17 +32,17 @@ class HHCurrentTraits(HasTraits):
     HKCur1 = Float(10)
     HssminCur1 = Float(0)
     Hdenom_expCur1 = Float(1)
-    HpCur1 = Float(1)
+    HpCur1 = Float(0)
 
-    ErevCur2 = Float(-60)
-    GmaxCur2 = Float(0.05)
-    MvhalfCur2 = Float(-80)
-    MKCur2 = Float(10)
+    ErevCur2 = Float(20)
+    GmaxCur2 = Float(0.08)
+    MvhalfCur2 = Float(-45)
+    MKCur2 = Float(-5)
     MssminCur2 = Float(0)
     Mdenom_expCur2 = Float(1)
     MpCur2 = Float(1)
 
-    NvhalfCur2 = Float(-80)
+    NvhalfCur2 = Float(25)
     NKCur2 = Float(10)
     NssminCur2 = Float(0)
     Ndenom_expCur2 = Float(1)
@@ -50,56 +52,69 @@ class HHCurrentTraits(HasTraits):
     HKCur2 = Float(10)
     HssminCur2 = Float(0)
     Hdenom_expCur2 = Float(1)
-    HpCur2 = Float(1)
-    
-    traits_view = View(
-        Group(Group(Item('gateplot', editor=ComponentEditor(), show_label=False),
-                    Group(Group(Group(Item('MvhalfCur1'),
-                                      Item('MKCur1'),
-                                      Item('MssminCur1'),
-                                      Item('Mdenom_expCur1'),
-                                      Item('MpCur1'),
-                                      orientation = "vertical"),
-                                Group(Item('NvhalfCur1'),
-                                      Item('NKCur1'),
-                                      Item('NssminCur1'),
-                                      Item('Ndenom_expCur1'),
-                                      Item('NpCur1'),
-                                      orientation = "vertical"),
-                                Group(Item('HvhalfCur1'),
-                                      Item('HKCur1'),
-                                      Item('HssminCur1'),
-                                      Item('Hdenom_expCur1'),
-                                      Item('HpCur1'),
-                                      orientation = "vertical")),
-                          Group(Group(Item('MvhalfCur2'),
-                                      Item('MKCur2'),
-                                      Item('MssminCur2'),
-                                      Item('Mdenom_expCur2'),
-                                      Item('MpCur2'),
-                                      orientation = "vertical"),
-                                Group(Item('NvhalfCur2'),
-                                      Item('NKCur2'),
-                                      Item('NssminCur2'),
-                                      Item('Ndenom_expCur2'),
-                                      Item('NpCur2'),
-                                      orientation = "vertical"),
-                                Group(Item('HvhalfCur2'),
-                                      Item('HKCur2'),
-                                      Item('HssminCur2'),
-                                      Item('Hdenom_expCur2'),
-                                      Item('HpCur2'),
-                                      orientation = "vertical")))),
-              orientation = "horizontal"),
-        Group(Item('ivplot', editor=ComponentEditor(), show_label=False),
-              Group(Item('ErevCur1'),
-                    Item('GmaxCur1'),
-                    Item('ErevCur2'),
-                    Item('GmaxCur2'),
-                    orientation = "vertical"),
-              orientation = "horizontal"))
+    HpCur2 = Float(0)
 
-    def __init__(self):
+    traits_view = View(
+        Group(
+            Item('plots', editor=ComponentEditor(), show_label=False),
+            Group(
+                Group(
+                    Group(
+                        Item('MvhalfCur1'),
+                        Item('MKCur1'),
+                        Item('MssminCur1'),
+                        Item('Mdenom_expCur1'),
+                        Item('MpCur1'),
+                        orientation = "vertical"),
+                    Group(
+                        Item('NvhalfCur1'),
+                        Item('NKCur1'),
+                        Item('NssminCur1'),
+                        Item('Ndenom_expCur1'),
+                        Item('NpCur1'),
+                        orientation = "vertical"),
+                    Group(
+                        Item('HvhalfCur1'),
+                        Item('HKCur1'),
+                        Item('HssminCur1'),
+                        Item('Hdenom_expCur1'),
+                        Item('HpCur1'),
+                        orientation = "vertical"),
+                    Group(
+                        Item('GmaxCur1'),
+                        Item('ErevCur1'),
+                        orientation = "vertical")),
+                Group(
+                    Group(
+                        Item('MvhalfCur2'),
+                        Item('MKCur2'),
+                        Item('MssminCur2'),
+                        Item('Mdenom_expCur2'),
+                        Item('MpCur2'),
+                        orientation = "vertical"),
+                    Group(
+                        Item('NvhalfCur2'),
+                        Item('NKCur2'),
+                        Item('NssminCur2'),
+                        Item('Ndenom_expCur2'),
+                        Item('NpCur2'),
+                        orientation = "vertical"),
+                    Group(
+                        Item('HvhalfCur2'),
+                        Item('HKCur2'),
+                        Item('HssminCur2'),
+                        Item('Hdenom_expCur2'),
+                        Item('HpCur2'),
+                        orientation = "vertical"),
+                    Group(
+                        Item('GmaxCur2'),
+                        Item('ErevCur2'),
+                        orientation = "vertical")),
+                Item('write_button'),
+                orientation = "horizontal"),
+            orientation = "horizontal"))
+    
+    def __init__(self, ExprmntVm=None, ExprmntnA=None):
 
         super(HHCurrentTraits, self).__init__()
         # gates
@@ -118,16 +133,27 @@ class HHCurrentTraits(HasTraits):
         Cur2gatesplot.plot(("x", "N"), type = "line", color = "green")
         Cur2gatesplot.plot(("x", "H"), type = "line", color = "red")
 
-        gateplot = VPlotContainer(Cur1gatesplot, Cur2gatesplot)
-        self.gateplot = gateplot
-
         (Cur1,Cur2) = self.__iv()
         self.ivdata = ArrayPlotData(x=self.vm, nA1=Cur1, nA2=Cur2, combin=Cur1+Cur2)
         ivplot = Plot(self.ivdata)
         ivplot.plot(("x", "nA1"), type = "line", color = "blue")
         ivplot.plot(("x", "nA2"), type = "line", color = "green")
         ivplot.plot(("x", "combin"), type = "line", color = "black")
-        self.ivplot = ivplot
+        
+        if ExprmntVm is not None:
+            self.ivdata.set_data('ExptVm',ExprmntVm)
+            self.ivdata.set_data('ExptnA',ExprmntnA)
+            ivplot.plot(("ExptVm", "ExptnA"),
+                        type = "scatter", color = "red", marker_size = 5)
+
+
+        self.plots = VPlotContainer(ivplot, Cur2gatesplot, Cur1gatesplot)
+        self.plots.spacing = 0
+        ivplot.padding_top = 0
+        Cur1gatesplot.padding_bottom = 0
+        Cur2gatesplot.padding_top = 0
+
+        self.write_button = Button(label="Print_Pars")
 
     def __gates(self):
         MCur1 = (1-self.MssminCur1)/(1 + np.exp((self.vm - self.MvhalfCur1)/self.MKCur1))**self.Mdenom_expCur1 + self.MssminCur1
@@ -136,6 +162,18 @@ class HHCurrentTraits(HasTraits):
         MCur2 = (1-self.MssminCur2)/(1 + np.exp((self.vm - self.MvhalfCur2)/self.MKCur2))**self.Mdenom_expCur2 + self.MssminCur2
         NCur2 = (1-self.NssminCur2)/(1 + np.exp((self.vm - self.NvhalfCur2)/self.NKCur2))**self.Ndenom_expCur2 + self.NssminCur2
         HCur2 = (1-self.HssminCur2)/(1 + np.exp((self.vm - self.HvhalfCur2)/self.HKCur2))**self.Hdenom_expCur2 + self.HssminCur2
+        if self.MpCur1==0:
+            MCur1 = np.repeat(0,len(self.vm))
+        if self.NpCur1==0:
+            NCur1 = np.repeat(0,len(self.vm))
+        if self.HpCur1==0:
+            HCur1 = np.repeat(0,len(self.vm))
+        if self.MpCur2==0:
+            MCur2 = np.repeat(0,len(self.vm))
+        if self.NpCur2==0:
+            NCur2 = np.repeat(0,len(self.vm))
+        if self.HpCur2==0:
+            HCur2 = np.repeat(0,len(self.vm))
         return ((MCur1,NCur1,HCur1),(MCur2, NCur2, HCur2))
 
     def __iv(self):
@@ -144,18 +182,8 @@ class HHCurrentTraits(HasTraits):
         Cur2 = (MCur2**self.MpCur2 * NCur2**self.NpCur2 * HCur2**self.HpCur2)*self.GmaxCur2*(self.vm - self.ErevCur2)
         return (Cur1,Cur2)
 
-    @on_trait_change('ErevCur1','GmaxCur1','MvhalfCur1',
-                     'MKCur1','MssminCur1','Mdenom_expCur1',
-                     'MpCur1','NvhalfCur1','NKCur1',
-                     'NssminCur1','Ndenom_expCur1','NpCur1',
-                     'HvhalfCur1','HKCur1','HssminCur1',
-                     'Hdenom_expCur1','HpCur1',
-                     'ErevCur2','GmaxCur2','MvhalfCur2',
-                     'MKCur2','MssminCur2','Mdenom_expCur2',
-                     'MpCur2','NvhalfCur2','NKCur2',
-                     'NssminCur2','Ndenom_expCur2','NpCur2',
-                     'HvhalfCur2','HKCur2','HssminCur2',
-                     'Hdenom_expCur2','HpCur2')
+    # '+' matches all traits on the object
+    @on_trait_change('+')
     def _calc_current(self):
         ((MCur1,NCur1,HCur1),(MCur2,NCur2,HCur2)) = self.__gates()
         (Cur1, Cur2) = self.__iv()
@@ -170,5 +198,24 @@ class HHCurrentTraits(HasTraits):
         self.ivdata.set_data("nA2", Cur2)
         self.ivdata.set_data("combin", comb)
 
+    def _write_button_fired(self):
+        with open('pars.txt', 'wt') as sys.stdout: self.print_traits()
+
+def main(atf_path):
+    ''' pass in the full path of an ATF file with the difference current IV'''
+    from atf_reader import ATFreader
+    atf = ATFreader(atf_path)
+    mV = atf.read_data()[:,0]
+    nA = atf.read_data()[:,1]
+    HHCurrentTraits(ExprmntVm = mV, ExprmntnA = nA).configure_traits()
+
 if __name__ == "__main__":
-    HHCurrentTraits().configure_traits()
+    ''' pass in the full path of an ATF file with the difference current IV'''
+    import sys
+    if len(sys.argv)<2:
+        print("need name of axon text file")
+        raise ValueError
+    main(sys.argv[1])
+
+
+
--- DynClmpSSHandFiter.py
+++ DynClmpSSHandFiter.py
@@ -2,15 +2,14 @@ from traits.api import HasTraits, Instance, Int, Float, on_trait_change
 from traitsui.api import View, Group, Item
 from enable.api import ColorTrait
 from enable.component_editor import ComponentEditor
-from chaco.api import marker_trait, Plot, ArrayPlotData
+from chaco.api import marker_trait, Plot, ArrayPlotData, VPlotContainer
 from numpy import linspace, sin
 import pdb
 import numpy as np
 
 class HHCurrentTraits(HasTraits):
 
-    gateplot = Instance(Plot)
-    ivplot = Instance(Plot)
+    plots = Instance(VPlotContainer)
 
     Erev = Float(-60)
     Gmax = Float(0.05)
@@ -33,31 +32,32 @@ class HHCurrentTraits(HasTraits):
     Hp = Float(1)
     
     traits_view = View(
-        Group(Group(Item('gateplot', editor=ComponentEditor(), show_label=False),
-                    Group(Item('Mvhalf'),
-                          Item('MK'),
-                          Item('Mssmin'),
-                          Item('Mdenom_exp'),
-                          Item('Mp'),
-                          orientation = "vertical"),
-                    Group(Item('Nvhalf'),
-                          Item('NK'),
-                          Item('Nssmin'),
-                          Item('Ndenom_exp'),
-                          Item('Np'),
-                          orientation = "vertical"),
-                    Group(Item('Hvhalf'),
-                          Item('HK'),
-                          Item('Hssmin'),
-                          Item('Hdenom_exp'),
-                          Item('Hp'),
-                          orientation = "vertical"),
-                    orientation = "horizontal"),
-              Group(Item('ivplot', editor=ComponentEditor(), show_label=False),
-                    Group(Item('Erev'),
-                          Item('Gmax'),
-                          orientation = "vertical"),
-                    orientation = "horizontal")))
+        Group(
+            Group(Item('plots', editor=ComponentEditor(), show_label=False),
+                  Group(Item('Mvhalf'),
+                        Item('MK'),
+                        Item('Mssmin'),
+                        Item('Mdenom_exp'),
+                        Item('Mp'),
+                        orientation = "vertical"),
+                  Group(Item('Nvhalf'),
+                        Item('NK'),
+                        Item('Nssmin'),
+                        Item('Ndenom_exp'),
+                        Item('Np'),
+                        orientation = "vertical"),
+                  Group(Item('Hvhalf'),
+                        Item('HK'),
+                        Item('Hssmin'),
+                        Item('Hdenom_exp'),
+                        Item('Hp'),
+                        orientation = "vertical"),
+                  Group(Item('Erev'),
+                        Item('Gmax'),
+                        orientation = "vertical"),
+                  orientation = "horizontal")
+            )
+        )
 
     def __init__(self):
 
@@ -74,11 +74,17 @@ class HHCurrentTraits(HasTraits):
         gateplot.plot(("x", "M"), type = "line", color = "blue")
         gateplot.plot(("x", "N"), type = "line", color = "green")
         gateplot.plot(("x", "H"), type = "line", color = "red")
-        self.gateplot = gateplot
 
         ivplot = Plot(self.ivdata)
         ivplot.plot(("x", "nA"), type = "line", color = "black")
-        self.ivplot = ivplot
+
+        container = VPlotContainer(ivplot, gateplot)
+        container.spacing = 0
+        gateplot.x_axis.orientation = "top"
+        gateplot.padding_bottom = 0
+        ivplot.padding_top = 0
+        
+        self.plots = container
 
     def __gates(self):
         M = (1-self.Mssmin)/(1 + np.exp((self.vm - self.Mvhalf)/self.MK))**self.Mdenom_exp + self.Mssmin
@@ -88,7 +94,7 @@ class HHCurrentTraits(HasTraits):
 
     def __iv(self):
         (M,N,H) = self.__gates()
-        return (M**self.Mp * N**self.Np * H**self.Hp)*self.Gmax*(self.vm - self.Erev)
+        return (M**self.Mp * N**self.Np * H**self.Hp) * self.Gmax * (self.vm - self.Erev)
 
     @on_trait_change('Erev','Gmax','Mvhalf',
                      'MK','Mssmin','Mdenom_exp',
@@ -103,6 +109,15 @@ class HHCurrentTraits(HasTraits):
         self.gatedata.set_data("N", N)
         self.gatedata.set_data("H", H)
         self.ivdata.set_data("nA", nA)
+
+class CompositeCurrent(HasTraits):
+
+    gates = Instance(VPlotContainer)
+    iv = Instance(Plot)
+
+    
+
+    
 
 if __name__ == "__main__":
     HHCurrentTraits().configure_traits()
